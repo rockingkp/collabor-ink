f89918f5ef326f54c7cf390adf98f49bd71d9836
rockingkp <kaamyakpant@gmail.com>
2024-07-22 15:20:30 +0530

    minor change

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,14 @@
 import "@/styles/globals.css";
 import { Provider } from "react-redux";
 import { store } from "./../store";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
+      <Head>
+        <title>Collabor Ink</title>
+      </Head>
       <Component {...pageProps} />
     </Provider>
   );
